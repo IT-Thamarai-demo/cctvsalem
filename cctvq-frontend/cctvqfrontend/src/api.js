86b126d8d360418472acf2b234e6f13041e57f01
Thamaraikannan <thamarair65@gmail.com>
2025-06-17 18:15:23 +0530

    Delete cctvq-frontend directory

--- cctvq-frontend/cctvqfrontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'https://cctvsalem.onrender.com' });
-
-export const fetchQuotations = () => API.get('/quotations');
-export const createQuotation = (data) => API.post('/quotations', data);
-export const updateQuotation = (id, data) => API.put(`/quotations/${id}`, data);
-export const deleteQuotation = (id) => API.delete(`/quotations/${id}`);
